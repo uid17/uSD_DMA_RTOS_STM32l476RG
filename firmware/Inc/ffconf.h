38caddf9acb955d2a91f6b4ac1d1938f658181c0
dmitry <dmitry.kochubey@tul.cz>
2018-05-09 10:55:21 +0200

    Original from STM #define _FS_REENTRANT    0 #define _SYNC_t NULL

--- firmware/Inc/ffconf.h
+++ firmware/Inc/ffconf.h
@@ -74,7 +74,7 @@
 /   2: f_opendir(), f_readdir() and f_closedir() are removed in addition to 1.
 /   3: f_lseek() function is removed in addition to 2. */
 
-#define _USE_STRFUNC         2      /* 0:Disable or 1-2:Enable */
+#define _USE_STRFUNC         0      /* 0:Disable or 1-2:Enable */
 /* This option switches string functions, f_gets(), f_putc(), f_puts() and
 /  f_printf().
 /
@@ -138,7 +138,7 @@
 /   950 - Traditional Chinese (DBCS)
 */
 
-#define _USE_LFN     2    /* 0 to 3 */
+#define _USE_LFN     3    /* 0 to 3 */
 #define _MAX_LFN     255  /* Maximum LFN length to handle (12 to 255) */
 /* The _USE_LFN switches the support of long file name (LFN).
 /
